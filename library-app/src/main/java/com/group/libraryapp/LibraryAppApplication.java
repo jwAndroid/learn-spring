af2e313ceae4b78fb2e36bf1ec74e44010031bd0
jwAndroid <cjd9408er@naver.com>
2023-05-23 15:34:03 +0900

    make project

--- library-app/src/main/java/com/group/libraryapp/LibraryAppApplication.java
+++ library-app/src/main/java/com/group/libraryapp/LibraryAppApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibraryAppApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(LibraryAppApplication.class, args);
   }
