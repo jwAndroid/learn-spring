d727f3938f195d933588c1bf0515462750758f91
jwAndroid <cjd9408er@naver.com>
2023-06-02 05:12:24 +0900

    영속성 컨텍스트와 트랜잭션

--- library-app/src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ library-app/src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -6,6 +6,8 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,20 +19,26 @@ public class UserServiceV2 {
         this.userRepository = userRepository;
     }
 
+    @Transactional
     public void saveUser(UserCreateRequest dto) {
         userRepository.save(new User(dto.getName() , dto.getAge()));
     }
 
+    @Transactional(readOnly = true)
     public List<UserResponse> getUsers() {
-        return userRepository.findAll().stream().map(UserResponse::new).collect(Collectors.toList());
+        return userRepository.findAll().stream()
+                .map(UserResponse::new)
+                .collect(Collectors.toList());
     }
 
+    @Transactional
     public void updateUser(UserUpdateRequest dto) {
         User user = userRepository.findById(dto.getId()).orElseThrow(IllegalAccessError::new);
         user.updateName(dto.getName());
-        userRepository.save(user);
+//        userRepository.save(user);
     }
 
+    @Transactional
     public void deleteUser(String name) {
         User user = userRepository.findByName(name).orElseThrow(IllegalArgumentException::new);
         userRepository.delete(user);
